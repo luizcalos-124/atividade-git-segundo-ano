74f3bf4d6d07bec2d4360e6d357feba7f4ace628
l <luizcarlos.tec1@gmail.com>
2026-05-07 16:47:54 -0300

    Update atividade1.py

--- Luiz carlos/atividade1.py
+++ Luiz carlos/atividade1.py
@@ -1,11 +1,33 @@
-idade = int(input("Digite a idade do aluno: "))
-autorizacao = input ("Possui autorização dos responsáveis? (sim ou não): ")
+print("////// Menu de jogo //////\n")
 
-if idade >=18:
-    print("Entrada permitida.")
-elif idade >= 16 and autorizacao == "sim":
-    print("Entrada permitida.")
-elif idade < 16 and autorizacao == "sim":
-    print("Entrada permitida apenas com responsável.")
-else:
-    print("Entrada não premitida no evento.")
+Estoque = [
+     
+]
+a = 0
+
+while a != 4:
+    print("/ 1- Cadastrar produto /")
+    print("/ 2- Quantidade /")
+    print("/ 3- Preço /")
+    print("/ 4- Sair /")
+
+a = int(input("Escolha um : "))
+
+if (a > 5) or (a < 1):
+     print("Número inválido! Tente novamente!")
+
+elif a == 1:
+        nome = input("Digite o nome do item: ")
+        quant = int(input("Digite a quantidade do item: "))
+        pre = float(input("Digite o valor do produto: "))
+
+if quant > 50:
+ estoque = "Estoque Alto" 
+elif quant >= 20 and quant <= 50:
+     estoque = "Estoque Médio" 
+elif quant < 20:
+    estoque =  "Estoque Baixo" 
+
+    produto = [nome,quant,pre]
+    Estoque.append[produto]
+    print()
